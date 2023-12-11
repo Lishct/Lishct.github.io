bdfc181a2dc6d14c09d964960fdb0a8fcf582b03
BurningSlap <147299181+Lishct@users.noreply.github.com>
2023-12-11 21:27:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function show () {
+    document.getElementById("exort").style.display = 'block';
+    document.getElementById("show").style.display = 'none';
+}
+function back () {
+    document.getElementById("exort").style.display = 'none';
+    document.getElementById("show").style.display = 'block';
+}
